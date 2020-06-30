b1929ecd04b7ee8d84eca997705b48d725214937
Joel Enikanorogbon <g.eniqs03@gmail.com>
2020-06-30 18:18:39 -0400

    Added Brand to product entity

---
+++ BorrowToOwn.API/BorrowToOwn.Migrations/Data/20200630174657_added brand entity.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace BorrowToOwn.API.BorrowToOwn.Migrations.Data
+{
+    public partial class addedbrandentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BrandId",
+                table: "Products",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Brand",
+                columns: table => new
+                {
+                    BrandId = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Brand", x => x.BrandId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_BrandId",
+                table: "Products",
+                column: "BrandId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Brand_BrandId",
+                table: "Products",
+                column: "BrandId",
+                principalTable: "Brand",
+                principalColumn: "BrandId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Brand_BrandId",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Brand");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_BrandId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "BrandId",
+                table: "Products");
+        }
+    }
+}
